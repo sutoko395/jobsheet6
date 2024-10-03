1ace60d8b8d72af25679dcb5a2566f87f9f12730
sutoko395 <sutokotokotoko@gmail.com>
2024-10-03 23:20:47 +0700

    perbarui file

--- Pemilihan2Percobaan123.java
+++ Pemilihan2Percobaan123.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 /**
- * Pemilihan2Percobaan123
+ * LeapYearChecker
  */
 public class Pemilihan2Percobaan123 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int tahun;
-
-        System.out.println("Masukkan Tahun");
+        
+        System.out.print("Masukkan tahun: ");
         tahun = sc.nextInt();
-
-
-        if ((tahun% 4) == 0) {
-            if ((tahun% 100) != 0 )
-                System.out.println("Tahun Kabisat"); 
-        } else
+        
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
-
-            sc.close();
+        }
+        sc.close();
     }
-
 }
